81b20fe6ff0d6b8cc417cbfc88f88946c37f685c
PraVeenxJSX <160173872+PraVeenxJSX@users.noreply.github.com>
2025-12-21 00:02:43 +0530

    changed url in frontend

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -3,7 +3,7 @@ import { io, Socket } from "socket.io-client";
 let socket: Socket | null = null;
 
 export const connectSocket = (token: string) => {
-  socket = io("http://localhost:5000", {
+  socket = io("https://chat-backend4.onrender.com", {
     query: { token },
   });
 
